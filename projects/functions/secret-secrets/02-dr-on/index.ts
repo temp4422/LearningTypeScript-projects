1aa2d7d38f1723f95d36e878bea4608a39638099
user <user@MacBook-Pro-user.local>
2024-03-24 16:03:10 +0200

    05. Functions: Secret Secrets - 2. Dr. On

--- projects/functions/secret-secrets/02-dr-on/index.ts
+++ projects/functions/secret-secrets/02-dr-on/index.ts
@@ -1,2 +1,25 @@
 // Write your createAdvancedCipher function here! ✨
 // You'll need to export it so the tests can run it.
+export type Cipher = (character: string) => string;
+
+export function createAdvancedCipher(
+	onVowel: Cipher,
+	onConsonant: Cipher,
+	onPunctuation: Cipher
+) {
+	return function (text: string) {
+		let returnString = "";
+
+		for (const character of text) {
+			if (character.match(/[aeiou]/i)) {
+				returnString += onVowel(character);
+			} else if (character.match(/[bcdfghjklmnpqrstvwxyz]/i)) {
+				returnString += onConsonant(character);
+			} else {
+				returnString += onPunctuation(character);
+			}
+		}
+
+		return returnString;
+	};
+}
