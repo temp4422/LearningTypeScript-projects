3a4d83eac439077e28d4966595c59f7fd7786b47
user <user@MacBook-Pro-user.local>
2024-03-24 16:34:54 +0200

    05. Functions: Secret Secrets - 3. The Golden Code ❌

--- projects/functions/secret-secrets/03-the-golden-code/index.ts
+++ projects/functions/secret-secrets/03-the-golden-code/index.ts
@@ -1,2 +1,22 @@
 // Write your createCodeCracker function here! ✨
 // You'll need to export it so the tests can run it.
+
+export type Settings = {
+	attempts: number;
+	makeGuess: (text: string, attempt: number) => string;
+	validateGuess: (guess: string) => boolean;
+};
+
+export function createCodeCracker({
+	attempts,
+	makeGuess,
+	validateGuess,
+}: Settings) {
+	return function (text: string) {
+		for (let i = 0; i < attempts; i++) {
+			const guess = makeGuess(text, i);
+			if (validateGuess(guess)) return guess;
+		}
+		return undefined;
+	};
+}
