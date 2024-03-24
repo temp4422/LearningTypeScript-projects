e2230a1ffd9746ecbba25ca6dbe47257ad916702
user <user@MacBook-Pro-user.local>
2024-03-24 15:49:17 +0200

    05. Functions: Secret Secrets - 1. Incoming Cipher

--- projects/functions/secret-secrets/01-incoming-cipher/index.ts
+++ projects/functions/secret-secrets/01-incoming-cipher/index.ts
@@ -1,2 +1,19 @@
 // Write your createCipher function here! ✨
 // You'll need to export it so the tests can run it.
+// function cipher(string: string): string {
+// 	return "string";
+// }
+
+export type Cipher = (string: string) => string;
+
+export function createCipher(cipher: Cipher) {
+	return (string: string) => {
+		let returnString = "";
+
+		for (let i = 0; i < string.length; i++) {
+			returnString += cipher(string[i]);
+		}
+
+		return returnString;
+	};
+}
