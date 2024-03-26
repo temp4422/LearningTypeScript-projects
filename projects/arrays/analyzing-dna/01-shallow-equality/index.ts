ce3a79c237be8d6cadd788e453aaa52ed744eb7d
user <user@MacBook-Pro-user.local>
2024-03-26 15:22:25 +0200

    06. Arrays: Analyzing DNA - 1. Shallow Equality

--- projects/arrays/analyzing-dna/01-shallow-equality/index.ts
+++ projects/arrays/analyzing-dna/01-shallow-equality/index.ts
@@ -1,2 +1,11 @@
 // Write your shallowEquality function here! ✨
 // You'll need to export it so the tests can run it.
+export function shallowEquality(a: string[], b: string[]): boolean {
+	// Return: A boolean indicating whether a and b contain the same strings.
+	if (a.length != b.length) return false;
+	for (let i = 0; i < a.length; i++) {
+		if (a[i] != b[i]) return false;
+	}
+	return true;
+}
+// shallowEquality(["a", "c", "g", "t"], ["a", "c", "g", "a"]);
