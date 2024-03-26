3fd20fed9a3ec800611714846fdf994f7069e4c7
user <user@MacBook-Pro-user.local>
2024-03-26 15:33:07 +0200

    06. Arrays: Analyzing DNA - 2. Shallow Differences

--- projects/arrays/analyzing-dna/02-shallow-differences/index.ts
+++ projects/arrays/analyzing-dna/02-shallow-differences/index.ts
@@ -1,2 +1,18 @@
 // Write your shallowDifferences function here! ✨
 // You'll need to export it so the tests can run it.
+export type MergeArray = (string | undefined)[];
+
+export function shallowDifferences(
+	a: string[],
+	b: string[]
+): MergeArray | undefined {
+	if (a.length != b.length) return undefined;
+
+	const newArr: MergeArray = [];
+	for (let i = 0; i < a.length; i++) {
+		if (a[i] != b[i]) newArr.push(undefined);
+		else newArr.push(a[i]);
+	}
+
+	return newArr;
+}
