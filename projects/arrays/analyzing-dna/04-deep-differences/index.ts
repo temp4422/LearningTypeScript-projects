75d2bf2b88546e248c0c4e287d88f91c2d008c42
user <user@MacBook-Pro-user.local>
2024-03-26 16:40:21 +0200

    06. Arrays: Analyzing DNA - 4. Deep Differences

--- projects/arrays/analyzing-dna/04-deep-differences/index.ts
+++ projects/arrays/analyzing-dna/04-deep-differences/index.ts
@@ -1,2 +1,24 @@
 // Write your deepDifferences function here! ✨
 // You'll need to export it so the tests can run it.
+export function deepDifferences(a: string[][], b: string[][]) {
+	if (a.length != b.length) return undefined;
+
+	const newArray: ((string | undefined)[] | undefined)[] = [];
+
+	for (let i = 0; i < a.length; i++) {
+		if (a[i].length !== b[i].length) {
+			newArray.push(undefined);
+		} else {
+			newArray.push([]);
+			for (let j = 0; j < a[i].length; j++) {
+				if (a[i][j] != b[i][j]) {
+					newArray[i]?.push(undefined);
+				} else {
+					newArray[i]?.push(a[i][j]);
+				}
+			}
+		}
+	}
+
+	return newArray;
+}
