b0fe1e9394c6a57c8ce60efb3c5940bafd013209
user <user@MacBook-Pro-user.local>
2024-03-27 21:37:50 +0200

    07. Interfaces: Vacation Planning - 2. Restaurants

--- projects/interfaces/vacation-planning/02-restaurants/index.ts
+++ projects/interfaces/vacation-planning/02-restaurants/index.ts
@@ -1,2 +1,34 @@
 // Write your groupRestaurants function here! ✨
 // You'll need to export it so the tests can run it.
+export interface SingleRestaurant {
+	city: string;
+	name: string;
+}
+
+export type Restaurants = SingleRestaurant[];
+
+export interface GroupedRestaurants {
+	[i: string]: string[];
+}
+
+export function groupRestaurants(restaurants: Restaurants) {
+	const result: GroupedRestaurants = {};
+
+	for (const { city, name } of restaurants) {
+		if (result[city]) {
+			result[city].push(name);
+		} else {
+			result[city] = [name];
+		}
+	}
+
+	return result;
+}
+
+const rests = [
+	{ city: "Troy", name: "Muddaddy Flats" },
+	{ city: "Albany", name: "Bombers" },
+	{ city: "Troy", name: "Nighthalks" },
+];
+
+groupRestaurants(rests);
