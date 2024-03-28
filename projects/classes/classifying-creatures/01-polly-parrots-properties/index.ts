5f308b1028f9ba1e4d0f4877d08f3dba379110f4
user <user@MacBook-Pro-user.local>
2024-03-28 19:29:07 +0200

    08. Classes: Classifying Creatures - 1. Polly Parrot's Properties

--- projects/classes/classifying-creatures/01-polly-parrots-properties/index.ts
+++ projects/classes/classifying-creatures/01-polly-parrots-properties/index.ts
@@ -1,2 +1,27 @@
 // Write your Parrot class here! ✨
 // You'll need to export it so the tests can run it.
+export class Parrot {
+	#name: string;
+	#phrase: string[];
+
+	constructor(name: string) {
+		this.#name = name;
+		this.#phrase = [`${this.#name} wants a cracker!`];
+	}
+
+	announce() {
+		return `Squawk! I'm ${this.#name}!`;
+	}
+	learn(newPhrase: string) {
+		this.#phrase.push(newPhrase);
+	}
+	speak() {
+		let randomIndex = Math.floor(Math.random() * this.#phrase.length);
+		return this.#phrase.at(randomIndex);
+	}
+}
+
+// const Polly = new Parrot("Polly");
+// console.log(Polly);
+// console.log(Polly.announce());
+// console.log(Polly.speak());
