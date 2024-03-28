985a11f69b5274bf57837325dd26d284afb22e5e
user <user@MacBook-Pro-user.local>
2024-03-28 21:13:53 +0200

    08. Classes: Classifying Creatures - 3. Hamster Helpers

--- projects/classes/classifying-creatures/03-hamster-helpers/index.ts
+++ projects/classes/classifying-creatures/03-hamster-helpers/index.ts
@@ -1,2 +1,50 @@
 // Write your type and classes here! ✨
 // You'll need to export the classes so the tests can run them.
+
+//prettier-ignore
+export type SmallPetFood = 'bugs'| 'fruits'| 'insects'| 'plants'| 'seeds'| 'vegetables'
+
+// 'abstract' create a base class that doesn’t itself declare the implementation of some methods, but instead expects a subclass to provide them.
+export abstract class SmallFurryPet {
+	readonly species: string;
+	protected happiness: number;
+
+	constructor(species: string) {
+		this.species = species;
+		this.happiness = 0;
+	}
+
+	abstract eats(food: SmallPetFood): boolean; // Not real just for comments
+
+	isHappy(): boolean {
+		return this.happiness > 0 ? true : false;
+	}
+}
+
+export class Gerbil extends SmallFurryPet {
+	constructor() {
+		super("Gerbil");
+	}
+
+	dig() {
+		this.happiness++;
+	}
+
+	eats(food: SmallPetFood) {
+		return ["insects", "plants", "seeds", "vegetables"].includes(food);
+	}
+}
+
+export class Hamster extends SmallFurryPet {
+	constructor() {
+		super("Hamster");
+	}
+
+	run() {
+		this.happiness++;
+	}
+
+	eats() {
+		return true;
+	}
+}
