485d9ce6a16b8c46f5cd8defb823f54f52f4b72b
user <user@MacBook-Pro-user.local>
2024-04-03 20:33:43 +0300

    08. Classes: Horror Factory ❌

--- projects/classes/horror-factory/src/index.ts
+++ projects/classes/horror-factory/src/index.ts
@@ -1,2 +1,5 @@
 // Write your class and functions here! ✨
 // You'll need to export the class and functions so the tests can run it.
+
+export function createDemon() {}
+export function createSorcerer() {}
