72eacc87889fefb67f4e0437bcc7b2cc42f9931f
user <user@MacBook-Pro-user.local>
2024-04-03 19:45:43 +0300

    10. Generics: Hidash - 1. Unique

--- projects/generics/hidash/01-unique/index.ts
+++ projects/generics/hidash/01-unique/index.ts
@@ -1,2 +1,15 @@
 // Write your unique function here! ✨
 // You'll need to export it so the tests can run it.
+export function unique<T>(...items: T[][]) {
+	const set = new Set();
+
+	for (const array of items) {
+		for (let i = 0; i < array.length; i++) {
+			set.add(array[i]);
+		}
+	}
+
+	return Array.from(set);
+}
+
+// unique(["a", "b"], ["b", "c", "a"]); // ['a', 'b', 'c']
