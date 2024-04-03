7a78448d70389c6521c491438874cc90eef11118
user <user@MacBook-Pro-user.local>
2024-04-03 20:03:18 +0300

    10. Generics: Hidash - 2. Zip

--- projects/generics/hidash/02-zip/index.ts
+++ projects/generics/hidash/02-zip/index.ts
@@ -1,2 +1,17 @@
 // Write your zip function here! ✨
 // You'll need to export it so the tests can run it.
+export function zip<A, B>(a: A[], b: B[]) {
+	const result: (A | B)[] = []; // alternating elements from the two arrays
+	let max = Math.max(a.length, b.length);
+	let i = 0;
+
+	while (i < max) {
+		if (a[i]) result.push(a[i]);
+		if (b[i]) result.push(b[i]);
+		i++;
+	}
+
+	return result;
+}
+
+// zip(["a", "b", "c", "d"], [1, 2]); // ['a', 1, 'b', 2, 'c', 'd']
