19651076ee345b2f9d3ab6c15c53ddfa772d89dc
user <user@MacBook-Pro-user.local>
2024-04-03 20:14:26 +0300

    10. Generics: Hidash - 4. Pick Many

--- projects/generics/hidash/04-pickmany/index.ts
+++ projects/generics/hidash/04-pickmany/index.ts
@@ -1,2 +1,13 @@
 // Write your pickMany function here! ✨
 // You'll need to export it so the tests can run it.
+export function pickMany<T, K extends keyof T>(container: T, keys: K[]) {
+	const result: T[K][] = [];
+
+	for (const key of keys) {
+		result.push(container[key]);
+	}
+
+	return result;
+}
+
+// pickMany({ a: 1, b: 2, c: 3 }, ["a", "c"]); // [1, 3]
