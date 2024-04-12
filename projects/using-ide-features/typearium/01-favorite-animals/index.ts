2d61e4f470304777c14cba1e4d3e364eab4dfe3b
user <user@MacBook-Pro-user.local>
2024-04-12 19:44:31 +0300

    12. Using IDE Features: Typearium - 1. Favorite Animals

--- projects/using-ide-features/typearium/01-favorite-animals/index.ts
+++ projects/using-ide-features/typearium/01-favorite-animals/index.ts
@@ -1,35 +1,27 @@
 // Refactor here! ✨
 
+const animals = [
+	"parakeet",
+	"macaw",
+	"cat",
+	"monkey",
+	"elephant",
+	"alpaca",
+	"fox",
+];
+
 export function checkIsAnyAnimalFavorite(...animals: string[]) {
-	const favoriteAnimalsUnique = new Set([
-		"parakeet",
-		"macaw",
-		"cat",
-		"monkey",
-		"elephant",
-		"alpaca",
-		"fox",
-	]);
+	const favoriteAnimalsUnique = new Set(animals);
 
 	return animals.some((animal) => favoriteAnimalsUnique.has(animal));
 }
 
 export function getFavoriteAnimals(max = Infinity) {
-	return [
-		"parakeet",
-		"macaw",
-		"cat",
-		"monkey",
-		"elephant",
-		"alpaca",
-		"fox",
-	].slice(0, max);
+	return animals.slice(0, max);
 }
 
 export function logFavoriteAnimals() {
-	["parakeet", "macaw", "cat", "monkey", "elephant", "alpaca", "fox"].forEach(
-		(animal, i) => {
-			console.log(`I like ${animal} number ${i}!`);
-		}
-	);
+	animals.forEach((animal, i) => {
+		console.log(`I like ${animal} number ${i}!`);
+	});
 }
