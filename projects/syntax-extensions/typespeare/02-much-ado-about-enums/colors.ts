28bfab9cb6e7371c91990d32094413b972f65e7d
user <user@MacBook-Pro-user.local>
2024-05-07 23:05:53 +0300

    14. Syntax Extensions: Typespeare - 2. Much Ado About Enums

--- projects/syntax-extensions/typespeare/02-much-ado-about-enums/colors.ts
+++ projects/syntax-extensions/typespeare/02-much-ado-about-enums/colors.ts
@@ -1,7 +1,5 @@
-export const Colors = {
-	Blue: "blue",
-	Red: "red",
-	Yellow: "yellow",
-} as const;
-
-export type Color = typeof Colors[keyof typeof Colors];
+export enum Colors {
+	Blue = "blue",
+	Red = "red",
+	Yellow = "yellow",
+}
